b110fa228ae382036ad38f30ea7a195764f80fd8
sanjana36 <72769989+sanjana36@users.noreply.github.com>
2023-10-09 19:55:19 -0400

    Created Drivetrain, ready to test driving

---
+++ Alana_robot1/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import frc.robot.commands.DefaultTeleopCommand;
+
+public class Drivetrain {
+
+    public void setDefaultCommand(DefaultTeleopCommand defaultTeleopCommand) {
+    }
+
+    public void drive(double leftY, double rightX) {
+    }
+
+}
--- Alana_robot1/src/main/java/frc/robot/RobotContainer.java
+++ Alana_robot1/src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.DefaultTeleopCommand;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -21,6 +22,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drivetrain drivetrain = new Drivetrain();
+  private DefaultTeleopCommand defaultTeleopCommand = null; 
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -28,6 +31,9 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    defaultTeleopCommand = new DefaultTeleopCommand(m_driverController, drivetrain);
+    drivetrain.setDefaultCommand(defaultTeleopCommand);
+    
     // Configure the trigger bindings
     configureBindings();
   }
