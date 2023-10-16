e7da06cd06f16ee9a91d272e9af09afb59c00f90
HeidiG2007 <78167256+HeidiG2007@users.noreply.github.com>
2023-10-16 19:42:41 -0400

    Import to 2022&Drivetrain reqmnt to default teleop

---
+++ EdwardandHarshad-Imported/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  private WPI_TalonSRX motorFrontLeft = new WPI_TalonSRX(10);
+  private WPI_TalonSRX motorBackLeft = new WPI_TalonSRX(11);
+  private WPI_TalonSRX motorFrontRight = new WPI_TalonSRX(13);
+  private WPI_TalonSRX motorBackRight = new WPI_TalonSRX(14);
+  private DifferentialDrive differentialDrive = new DifferentialDrive(motorFrontLeft, motorFrontRight);
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    motorBackLeft.follow(motorFrontLeft);
+    motorBackRight.follow(motorFrontRight);
+
+    motorFrontRight.setInverted(true);
+    motorBackRight.setInverted(true);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    motorFrontLeft.feed();
+    motorFrontRight.feed();
+    motorBackLeft.feed();
+    motorBackRight.feed();
+  }
+
+  public void drive(double xSpeed, double zRotation) {
+    differentialDrive.arcadeDrive(xSpeed, zRotation);
+  }
+}
