07e1a005130750c3c3f2bd7b3855d4e3c2898873
VSingh1012 <72770416+VSingh1012@users.noreply.github.com>
2023-10-19 20:02:27 -0400

    drive straight auton

---
+++ EdwardandHarshad/src/main/java/frc/robot/commands/DriveStraightEncoder.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class DriveStraightEncoder extends CommandBase {
+  private Drivetrain drivetrain = null;
+  private double distance = 0;
+  private double startPos = 0;
+
+  /** Creates a new DriveStraightEncoder. */
+  public DriveStraightEncoder(Drivetrain drivetrain, double distance) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
+    this.drivetrain = drivetrain;
+    this.distance = distance;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    startPos = drivetrain.getPosition();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drivetrain.autonDrive(0.3, 0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivetrain.autonDrive(0,0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return (drivetrain.getPosition() - startPos >= distance);
+  }
+}
