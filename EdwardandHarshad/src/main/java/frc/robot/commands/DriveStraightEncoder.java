6d1cab63bb7a314310178bb9ae074aa4e4d5d04b
MC Ride <hackbots3414@gmail.com>
2023-10-19 20:06:13 -0400

    encoder

--- EdwardandHarshad/src/main/java/frc/robot/commands/DriveStraightEncoder.java
+++ EdwardandHarshad/src/main/java/frc/robot/commands/DriveStraightEncoder.java
@@ -7,23 +7,22 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
-public class DriveStraight extends CommandBase {
+public class DriveStraightEncoder extends CommandBase {
   private Drivetrain drivetrain = null;
-  private long timeToDrive = 0;
-  private long startTime = 0;
-
-  /** Creates a new DriveStraight. */
-  public DriveStraight(Drivetrain drivetrain, long timeToDrive) {
+  private double distance = 0;
+  private double startPosition = 0;
+  /** Creates a new DriveStraightEncoder. */
+  public DriveStraightEncoder(Drivetrain drivetrain, double distanceToDrive) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drivetrain);
     this.drivetrain = drivetrain;
-    this.timeToDrive = timeToDrive;
+    this.distance = distance;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    startTime = System.currentTimeMillis();
+    startPosition = drivetrain.getPosition();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -35,12 +34,12 @@ public class DriveStraight extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    drivetrain.drive(0, 0);
+    drivetrain.autonDrive(0, 0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (System.currentTimeMillis() - startTime  < timeToDrive);
+    return (drivetrain.getPosition() - startPosition >= distance);
   }
 }
