6f3ef439cbd4bcfc06c73916a01aa51407a7bd29
julia_i <julia.d.ionescu@gmail.com>
2023-10-19 20:02:03 -0400

    autonDrive

--- EdwardandHarshad/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ EdwardandHarshad/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import javax.print.attribute.standard.RequestingUserName;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -15,6 +17,7 @@ public class Drivetrain extends SubsystemBase {
   private WPI_TalonFX motorFrontRight = new WPI_TalonFX(13);
   private WPI_TalonFX motorBackRight = new WPI_TalonFX(14);
   private DifferentialDrive differentialDrive = new DifferentialDrive(motorFrontLeft, motorFrontRight);
+
   /** Creates a new Drivetrain. */
   public Drivetrain() {
     motorBackLeft.follow(motorFrontLeft);
@@ -35,5 +38,15 @@ public class Drivetrain extends SubsystemBase {
 
   public void drive(double xSpeed, double zRotation) {
     differentialDrive.arcadeDrive(xSpeed, zRotation);
+  }
+
+  public void autonDrive(double xSpeed, double zRotation) {
+    differentialDrive.arcadeDrive(xSpeed, zRotation, false);
+  }
+
+  public double getPosition() {
+    double average = (motorFrontLeft.getSelectedSensorPosition() + motorFrontRight.getSelectedSensorPosition()
+        + motorBackLeft.getSelectedSensorPosition() + motorBackRight.getSelectedSensorPosition()) / 4;
+        return average;
   }
 }
