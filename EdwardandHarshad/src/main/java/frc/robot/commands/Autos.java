86cd72bf550a05fd921d1eefa61eb2fff1f19d10
bczdog <bczdog@gmail.com>
2023-10-26 19:43:43 -0400

    Fixed drive backwards bug

--- EdwardandHarshad/src/main/java/frc/robot/commands/Autos.java
+++ EdwardandHarshad/src/main/java/frc/robot/commands/Autos.java
@@ -31,6 +31,7 @@ public final class Autos {
     autonList.setDefaultOption("Do Nothing", new WaitCommand(5.0));
     autonList.addOption("Drive Straight 5 sec.", driveStraightByTime(drivetrain, 5000));
     autonList.addOption("Drive Straight 100K ticks", driveStraightByDistance(drivetrain, 100000));
+    autonList.addOption("Drive Straight -100K ticks", driveStraightByDistance(drivetrain, -100000));
     return autonList;
   }
 
