309e071060ac4f7bdabba52b8312adf863262cde
HeidiG2007 <78167256+HeidiG2007@users.noreply.github.com>
2023-10-30 19:55:04 -0400

    Modify talonFX device number

--- EdwardandHarshad/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ EdwardandHarshad/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
   private WPI_TalonFX motorFrontLeft = new WPI_TalonFX(10);
-  private WPI_TalonFX motorBackLeft = new WPI_TalonFX(11);
+  private WPI_TalonFX motorBackLeft = new WPI_TalonFX(12);
   private WPI_TalonFX motorFrontRight = new WPI_TalonFX(13);
   private WPI_TalonFX motorBackRight = new WPI_TalonFX(14);
   private DifferentialDrive differentialDrive = new DifferentialDrive(motorFrontLeft, motorFrontRight);
