0b1e752c108115033b6181e38c3fece0a12340d5
Vijay N <vijaynannapuraju@gmail.com>
2023-11-02 19:42:05 -0400

    added inchestoticks

--- EdwardandHarshad/src/main/java/frc/robot/commands/DriveStraightEncoder.java
+++ EdwardandHarshad/src/main/java/frc/robot/commands/DriveStraightEncoder.java
@@ -1,10 +1,11 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+// asdfasdfad
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.Constants.DriveStraightConstants;
 import frc.robot.subsystems.Drivetrain;
 
@@ -14,11 +15,11 @@ public class DriveStraightEncoder extends CommandBase {
   private double startPos = 0.0;
 
   /** Creates a new DriveStraightEncoder. */
-  public DriveStraightEncoder(Drivetrain drivetrain, double distanceToDrive) {
+  public DriveStraightEncoder(Drivetrain drivetrain, double inchesToDrive) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drivetrain);
     this.drivetrain = drivetrain;
-    this.distanceToDrive = distanceToDrive;
+    this.distanceToDrive = Constants.inchesToTicks(inchesToDrive);
   }
 
   // Called when the command is initially scheduled.
