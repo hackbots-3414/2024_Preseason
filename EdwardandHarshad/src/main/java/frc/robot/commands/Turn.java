bb879f07ef16a9f47a33210e9e8559e84aecfd25
julia_i <julia.d.ionescu@gmail.com>
2023-11-09 18:58:55 -0500

    Turn

--- EdwardandHarshad/src/main/java/frc/robot/commands/Turn.java
+++ EdwardandHarshad/src/main/java/frc/robot/commands/Turn.java
@@ -4,33 +4,36 @@
 
 package frc.robot.commands;
 
+import javax.lang.model.util.ElementScanner14;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
 public class Turn extends CommandBase {
   private Drivetrain drivetrain = null;
-  private double degrees = 0;
+  private double targetAngleDegrees = 0;
 
   /** Creates a new Turn. */
-  public Turn(Drivetrain drivetrain, double degrees) {
+  public Turn(Drivetrain drivetrain, double targetAngleDegrees) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drivetrain);
     this.drivetrain = drivetrain;
-    this.degrees = degrees;
+    this.targetAngleDegrees = targetAngleDegrees;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    drivetrain.resetYaw();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (degrees < 0) {
-      drivetrain.drive(0, -0.15);
+    if (targetAngleDegrees < 0) {
+      drivetrain.autonDrive(0, -0.15);
     } else {
-      drivetrain.drive(0, 0.15);
+      drivetrain.autonDrive(0, 0.15);
     }
   }
 
@@ -43,6 +46,7 @@ public class Turn extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
-  }
-}
+    double currentAngle = drivetrain.getTurnAngle();
+    return (targetAngleDegrees > 0 && targetAngleDegrees - currentAngle <= 0) ||
+    (targetAngleDegrees < 0 && targetAngleDegrees - currentAngle >= 0);
+  }}
