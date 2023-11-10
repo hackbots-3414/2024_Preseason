aaaf71d945d21daa467aa54150ae1ba9b0575e23
julia_i <julia.d.ionescu@gmail.com>
2023-11-09 19:51:08 -0500

    PIDTurn

---
+++ EdwardandHarshad/src/main/java/frc/robot/commands/PIDTurn.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.Drivetrain;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class PIDTurn extends PIDCommand {
+  private Drivetrain drivetrain = null;
+
+  /** Creates a new PIDTurn. */
+  public PIDTurn(Drivetrain drivetrain, double targetAngleDegrees) {
+    super(
+        // The controller that the command will use
+        new PIDController(0.3, 0, 0),
+        // This should return the measurement
+        drivetrain::getTurnAngle,
+        // This should return the setpoint (can also be a constant)
+        () -> targetAngleDegrees,
+        // This uses the output
+        output -> {
+          // Use the output here
+          drivetrain.autonDrive(0, output);
+        });
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure additional PID options by calling `getController` here.
+    addRequirements(drivetrain);
+    this.drivetrain = drivetrain;
+    getController().enableContinuousInput(-180, 180);
+    getController().setTolerance(2);
+    SmartDashboard.putData("PIDTurn Controller", getController());
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+    drivetrain.resetYaw();
+    getController().reset();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return getController().atSetpoint();
+  }
+}
