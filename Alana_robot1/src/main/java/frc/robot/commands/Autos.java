065badb27b6ab5c96a5546356def564b694a9540
MC Ride <hackbots3414@gmail.com>
2023-11-13 19:57:24 -0500

    Edited Autos

--- Alana_robot1/src/main/java/frc/robot/commands/Autos.java
+++ Alana_robot1/src/main/java/frc/robot/commands/Autos.java
@@ -40,8 +40,19 @@ public final class Autos {
 
   public static CommandBase createHealthToPhysicsAndBackAgain(Drivetrain drivetrain)
   {
-    //FiXME implement this sequence, replacing the WaitCommand
-    return new WaitCommand(10);
+  return new DriveStraightEncoder(drivetrain, 176).andThen(
+    new Turn(drivetrain, -45),
+    new DriveStraightEncoder(drivetrain, 156),
+    new Turn(drivetrain, -45),
+    new DriveStraightEncoder(drivetrain, 120),
+    new Turn(drivetrain, -90),
+    new DriveStraightEncoder(drivetrain, 105),
+    new Turn(drivetrain, -90),
+    new DriveStraightEncoder(drivetrain, 125),
+    new Turn(drivetrain, 45),
+    new DriveStraightEncoder(drivetrain, 100),
+    new Turn(drivetrain, 45),
+    new DriveStraightEncoder(drivetrain, 156));
   }
 
   public static SendableChooser<Command> buildAutonPicker(Drivetrain drivetrain) {
